0278df0cacea2e370d985d089c5c0b81853d68a2
Maximilian Seesslen <dev@seesslen.net>
2026-04-18 16:10:22 +0200

    Added addAllCommands() function
    Mostly for testing. Avoid bloatet main files.

---
+++ include/fosh/allCommands.hpp
@@ -0,0 +1,30 @@
+#ifndef FOSH_ALL_COMMAND_HPP
+#define FOSH_ALL_COMMAND_HPP
+/**---------------------------------------------------------------------------
+ *
+ * @file       all.hpp
+ * @brief      Add all available addable commands to an fosh instance
+ *
+ *             Just for testing
+ *
+ * @date       20260416
+ * @author     Maximilian Seesslen <mes@seesslen.net>
+ * @copyright  SPDX-License-Identifier: Apache-2.0
+ *
+ *--------------------------------------------------------------------------*/
+
+
+/*--- Includes -------------------------------------------------------------*/
+
+
+#include <fosh/fosh.hpp>
+
+
+/*--- Declaration ----------------------------------------------------------*/
+
+
+void addAllCommands( CFosh* fosh );
+
+
+/*--- Fin ------------------------------------------------------------------*/
+#endif // ? FOSH_ALL_COMMAND_HPP
---
+++ src/allCommands.cpp
@@ -0,0 +1,45 @@
+/**---------------------------------------------------------------------------
+ *
+ * @file       allCommands.cpp
+ * @brief      Add all available addable commands to an fosh instance
+ *
+ *             Just for testing
+ *
+ * @date       20260416
+ * @author     Maximilian Seesslen <mes@seesslen.net>
+ * @copyright  SPDX-License-Identifier: Apache-2.0
+ *
+ *--------------------------------------------------------------------------*/
+
+
+/*--- Includes -------------------------------------------------------------*/
+
+
+#include <fosh/fosh.hpp>
+#include <fosh/command.hpp>
+#include <lepto/lepto.h>               // IS_ENABLED
+#include <fosh/commands/biwak/date.hpp>
+#include <fosh/commands/biwak/reset.hpp>
+#include <fosh/commands/biwak/stop.hpp>
+#include <fosh/commands/log.hpp>
+#include <fosh/commands/biwak/standby.hpp>
+#include <fosh/commands/biwak/alarm.hpp>
+#include <fosh/allCommands.hpp>
+#include <biwak/rtc.hpp>
+
+
+/*--- Implementation -------------------------------------------------------*/
+
+
+void addAllCommands( CFosh* pFosh )
+{
+   pFosh->addCommand( new CCommandDate( "date", *libcBiwakRtc ) );
+   pFosh->addCommand( new CCommandReset( "reset" ) );
+   pFosh->addCommand( new CCommandStop( "stop" ) );
+   pFosh->addCommand( new CCommandLog( "log" ) );
+   pFosh->addCommand( new CCommandStandby( "standby" ) );
+   pFosh->addCommand( new CCommandAlarm( "alarm", *libcBiwakRtc ) );
+}
+
+
+/*--- Fin ------------------------------------------------------------------*/
