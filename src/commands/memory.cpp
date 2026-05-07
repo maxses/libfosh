76aa26d3bc8e39b60006d6d0f358a8d93e57eec0
Maximilian Seesslen <dev@seesslen.net>
2026-05-07 22:09:36 +0200

    Command 'memory': Make output nicely formated

--- src/commands/memory.cpp
+++ src/commands/memory.cpp
@@ -29,17 +29,19 @@
 int CCommandMemory::exec(int argc, const char *argv[]) const /* virtual override */
 {
    register long sp asm("sp");
-
-   #if STM32
-      printf("Heap start: %p\n", &__heap_start );
-      printf("Heap end  : %p\n", &__heap_end );
-   #endif
-      printf("Stack br    %p\n", &stackBarrier );
-   #if STM32
+   
+   #if defined(STM32)
       printf("End RAM   : %p\n", &_eram);
    #endif
-      printf("sbrk : %p\n", sbrk(0));
-      printf("sp   : %p\n", (void*)sp);
+      printf("SP        : %p\n", (void*)sp);
+      printf("Stack br  : %p\n", &stackBarrier );
+   #if defined( STM32 )
+      printf("Heap end  : %p\n", &__heap_end );
+   #endif
+      printf("sbrk      : %p\n", sbrk(0));
+   #if defined( STM32 )
+      printf("Heap start: %p\n", &__heap_start );
+   #endif
 
    return(0);
 }
