76659310901125b260941a025d499137130f101b
Maximilian Seesslen <dev@seesslen.net>
2026-06-08 10:31:28 +0200

    Added command 'candump'

---
+++ include/fosh/commands/biwak/candump.hpp
@@ -0,0 +1,39 @@
+#ifndef FOSH_COMMAND_CANDUMP_HPP
+#define FOSH_COMMAND_CANDUMP_HPP
+/**---------------------------------------------------------------------------
+ *
+ * @file       candump.hpp
+ * @brief      Libfosh command for dumping CAN traffic
+ *
+ *  \date      20260607
+ *  \author    Maximilian Seesslen <src@seesslen.net>
+ *  \copyright SPDX-License-Identifier: Apache-2.0
+ *
+ *--------------------------------------------------------------------------*/
+
+
+/*--- Includes -------------------------------------------------------------*/
+
+
+#include <fosh/command.hpp>
+#include <stdlib.h>
+#include <hwinfo/hwinfo.h>
+
+
+/*--- Declaration ----------------------------------------------------------*/
+
+
+class CCan;
+
+class CCommandCanDump: public CCommand
+{
+      CCan &m_can;
+      
+   public:
+      CCommandCanDump(const char *name, CCan &eeprom);
+      virtual int exec(int argc, const char *argv[]) const;
+};
+
+
+/*--- Fin ------------------------------------------------------------------*/
+#endif // ? ! FOSH_COMMAND_CANDUMP_HPP
---
+++ src/commands/biwak/candump.cpp
@@ -0,0 +1,68 @@
+/**---------------------------------------------------------------------------
+ *
+ * @file       candump.cpp
+ * @brief      Libfosh command for dumping CAN traffic
+ *
+ *  \date      20260607
+ *  \author    Maximilian Seesslen <src@seesslen.net>
+ *  \copyright SPDX-License-Identifier: Apache-2.0
+ *
+ *--------------------------------------------------------------------------*/
+
+
+/*--- Includes -------------------------------------------------------------*/
+
+
+#include <fosh/commands/biwak/candump.hpp>
+#include <lepto/log.h>
+#include <biwak/can.hpp>
+
+
+/*--- Implementation--------------------------------------------------------*/
+
+
+CCommandCanDump::CCommandCanDump(const char *name, CCan &can)
+   :CCommand( name, "Dump CAN traffic" )
+   ,m_can(can)
+{
+}
+
+
+int CCommandCanDump::exec(int argc, const char *argv[]) const /* virtual */
+{
+   int in;
+   
+   if( argc != 1 )
+   {
+      if( ! strcmp( argv[1], "-w" ) )
+      {
+         SCanMessage message;
+         printf("Send message\n");
+         m_can.transmit( message );
+      }
+   }
+   else
+   {
+      while( ( in = getc(stdin) ) == EOF )
+      {
+         SCanMessage *message;
+         
+         while( ( message=m_can.m_rxBuffer.frontEntry() ) )
+         {
+            printf( "  can0  %3X   [%d]  ", message->getId(), message->getLen() );
+            for(int i1=0; i1<message->getLen(); i1++)
+            {
+               printf( "%02X ", message->getData()[i1]);
+            }
+            printf( "\n" );
+            
+            m_can.m_rxBuffer.dropFront();
+         }
+      }
+   }
+   
+   return(0);
+}
+
+
+/*--- Fin ------------------------------------------------------------------*/
